06a5a593f866cfaed88b9f94911216d8fccf6f3f
spencervenancio <spencervenancio@gmail.com>
2026-04-26 15:40:15 -0500

    removed import of data, left as a param

--- src/models/logit.py
+++ src/models/logit.py
@@ -1,7 +1,5 @@
 def logistic_regression(X_train, y_train):
     from sklearn.linear_model import LogisticRegression
-    from src.utils import load_mnist
-    X_train, _, y_train, _ = load_mnist()
     clf = LogisticRegression(max_iter=300, solver='lbfgs')
     clf.fit(X_train, y_train)
     
