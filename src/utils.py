a0089dd2acdb0bdb96fdac950d776e8492ad389b
spencervenancio <spencervenancio@gmail.com>
2026-04-26 14:55:27 -0500

    Added load_mnist function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def get_patches(patch_size, img_size=28):
     """Returns list of patch index arrays for flattened img_size x img_size vector."""
@@ -12,3 +13,18 @@ def get_patches(patch_size, img_size=28):
             ]
             patches.append(np.array(idx))
     return patches
+
+def load_mnist():
+    from torchvision import datasets
+    
+    train = datasets.MNIST(root='data', train=True, download=True)
+    test = datasets.MNIST(root='data', train=False, download=True)
+
+
+    X_train = train.data.numpy().reshape(60000, -1) / 255.0  # (60000, 784)
+    y_train = train.targets.numpy()                           # (60000,)
+
+    X_test  = test.data.numpy().reshape(10000, -1) / 255.0   # (10000, 784)
+    y_test  = test.targets.numpy()                            # (10000,)
+    
+    return X_train, X_test, y_train, y_test
